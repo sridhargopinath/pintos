43c2547e4408d924d224c4b2bf801f504c44df5b
Sridhar Gopinath <g.sridhar53@gmail.com>
2015-11-01 16:36:52 +0530

    Basic functionailty of mmap done

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -106,6 +106,8 @@ static int argsNum[] = {
 	2,			// SYS_SEEK
 	1,			// SYS_TELL
 	1,			// SYS_CLOSE
+	2,			// SYS_MMAP
+	1			// SYS_UNMAP
 } ;
 
 // Exit the OS by just calling the shutdown function
@@ -263,7 +265,7 @@ bool remove ( const char *file )
 // Opens an already existing file
 int open ( const char *file )
 {
-	printf ( "Inside open\n" ) ;
+	/*printf ( "Inside open\n" ) ;*/
 	// Check the validity of the address of the file and also the filename
 	check_file ( (uint8_t *)file ) ;
 
@@ -314,7 +316,7 @@ int open ( const char *file )
 
 	lock_release ( &file_lock ) ;
 
-	printf ( "Fininsh open\n");
+	/*printf ( "Fininsh open\n");*/
 	return info->fd ;
 }
 
@@ -444,67 +446,68 @@ void close ( int fd )
 
 mapid_t mmap ( int fd, void *addr )
 {
-	printf ( "Inside mmap\n" ) ;
-	return -1 ;
-	/*struct thread *cur = thread_current() ;*/
+	/*printf ( "Inside mmap\n" ) ;*/
+	/*return -1 ;*/
+	struct thread *cur = thread_current() ;
 
-	/*if ( pg_ofs(addr) != 0 )*/
-		/*return -1 ;*/
+	if ( pg_ofs(addr) != 0 )
+		return -1 ;
 
-	/*if ( is_user_vaddr(addr) == false )*/
-		/*return -1 ;*/
+	if ( is_user_vaddr(addr) == false )
+		return -1 ;
 
-	/*struct file_info *file_info = get_file_info(fd) ;*/
-	/*if ( file_info == NULL )*/
-		/*return -1 ;*/
+	struct file_info *file_info = get_file_info(fd) ;
+	if ( file_info == NULL )
+		return -1 ;
 
 	/*printf ("Before file_length\n");*/
-	/*lock_acquire(&fd_lock) ;*/
-	/*int size = file_length(file_info->file) ;*/
-	/*lock_release(&fd_lock) ;*/
+	lock_acquire(&fd_lock) ;
+	int size = file_length(file_info->file) ;
+	lock_release(&fd_lock) ;
 
-	/*if ( size == 0 )*/
-		/*return -1 ;*/
+	if ( size == 0 )
+		return -1 ;
 
-	/*int read_bytes = size ;*/
-	/*int page_read_bytes, ofs = 0 ;*/
-	/*while ( read_bytes > 0 )*/
-	/*{*/
-		/*struct page *p = (struct page *) malloc ( sizeof(struct page));*/
-		/*if ( p == NULL )*/
-			/*PANIC("MMAP: SPTE memory allocation failed\n");*/
+	int read_bytes = size ;
+	int page_read_bytes, ofs = 0 ;
+	while ( read_bytes > 0 )
+	{
+		struct page *p = (struct page *) malloc ( sizeof(struct page));
+		if ( p == NULL )
+			PANIC("MMAP: SPTE memory allocation failed\n");
 
-		/*page_read_bytes = read_bytes < PGSIZE ? read_bytes : PGSIZE ;*/
+		page_read_bytes = read_bytes < PGSIZE ? read_bytes : PGSIZE ;
 
 		/*printf ("inside loop of mmap\n");*/
-		/*p->file = file_info->file ;*/
-		/*p->addr = addr ;*/
-		/*p->ofs = ofs ;*/
-		/*p->read_bytes = page_read_bytes ;*/
-		/*p->writable = true ;*/
-		/*p->kpage = NULL ;*/
+		p->file = file_info->file ;
+		p->addr = addr ;
+		p->ofs = ofs ;
+		p->read_bytes = page_read_bytes ;
+		p->writable = true ;
+		p->kpage = NULL ;
 
-		/*page_insert ( &cur->pages, &p->hash_elem ) ;*/
+		page_insert ( &cur->pages, &p->hash_elem ) ;
 
-		/*read_bytes -= page_read_bytes ;*/
-		/*ofs += page_read_bytes ;*/
-		/*addr += PGSIZE ;*/
-	/*}*/
+		read_bytes -= page_read_bytes ;
+		ofs += page_read_bytes ;
+		addr += PGSIZE ;
+	}
 	
-	/*[>struct map_info *newMap = (struct map_info *) malloc (sizeof(struct map_info));<]*/
-	/*[>if ( newMap == NULL )<]*/
-		/*[>PANIC("MMAP: Failed to allocate memory for map_info structure\n");<]*/
+	/*struct map_info *newMap = (struct map_info *) malloc (sizeof(struct map_info));*/
+	/*if ( newMap == NULL )*/
+		/*PANIC("MMAP: Failed to allocate memory for map_info structure\n");*/
 
-	/*[>newMap->mapid = allocateMAPID() ;<]*/
-	/*[>newMap->file = file ;<]*/
+	/*newMap->mapid = allocateMAPID() ;*/
+	/*newMap->file = file ;*/
 
-	/*mapid_t mapid = allocateMAPID() ;*/
+	mapid_t mapid = allocateMAPID() ;
 	/*printf ("return\n");*/
-	/*return mapid ;*/
+	return mapid ;
 }
 
 void munmap ( int mapping )
 {
+
 	return ;
 }
 
@@ -627,7 +630,7 @@ static void syscall_handler (struct intr_frame *f)
   for ( i = 0 ; i < n ; i ++ )
 	  pargs[i] = (void *) get_word_user ( (int*)f->esp + i + 1 ) ;
 
-  printf ( "sysnum is %d\n", sysNum ) ;
+  /*printf ( "sysnum is %d\n", sysNum ) ;*/
   switch ( sysNum )
   {
 	  case SYS_HALT:		halt () ;
@@ -669,7 +672,7 @@ static void syscall_handler (struct intr_frame *f)
 	  case SYS_CLOSE:		close ( (int)pargs[0] ) ;
 							break ;
 
-	  case SYS_MMAP:		printf ( "mmap called\n");
+	  case SYS_MMAP:		//printf ( "mmap called\n");
 							f->eax = mmap ( (int)pargs[0], (void *)pargs[1] ) ;
 							break ;
 
